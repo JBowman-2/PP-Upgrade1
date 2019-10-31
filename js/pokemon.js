4ab7d996db0b45b8508da2aa7c3053535b2d820f
Josh Bowman <10843511@my.uvu.edu>
2019-10-31 10:35:03 -0600

    figuring it out

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,4 +1,4 @@
-async function getPokemon(url) {
+async function getAPIData(url) {
     try {
         const responce = await fetch(url)
         const data = await responce.json()
@@ -6,7 +6,22 @@ async function getPokemon(url) {
     }
 
     catch(error) {
-        connsole.error(error)
+        console.error(error)
     }
 
 }
+
+const theData = getAPIDFata('https://pokeapi.com/api/v2/pokemon/')
+.then(data => {
+
+    for (const pokemon of data.results) {
+        getAPIData(pokemon.url)
+        .then(pokedata => {
+            console.log(pokedata)
+        })
+
+    }
+    
+})
+
+console.log(theData)
